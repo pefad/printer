54228d81785cc422de6df1bebdfe88a3373e43dc
unknown <opeyemifadiran@gmail.com>
2025-04-09 18:04:19 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bt-printer-app',
+  webDir: 'dist'
+};
+
+export default config;
